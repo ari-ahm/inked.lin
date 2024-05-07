534a3b2c868e4c68c9c4e654d72ab74ae8e1ae15
mohammad arian ahmidi <arian.ahmidi@gmail.com>
2024-05-07 23:41:58 +0330

    Initial commit

---
+++ inkout/src/main/java/ir/doodmood/inkout/Main.java
@@ -0,0 +1,7 @@
+package ir.doodmood.inkout;
+
+public class Main {
+    public static void main(String[] args) {
+        ir.doodmood.mashtframework.Main.main(args);
+    }
+}
---
+++ mashtframework/src/main/java/ir/doodmood/mashtframework/Main.java
@@ -0,0 +1,7 @@
+package ir.doodmood.mashtframework;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

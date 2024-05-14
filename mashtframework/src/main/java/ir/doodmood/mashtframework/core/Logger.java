c5dc2bd80c2b10c9153af335d82811772cf93df4
mohammad arian ahmidi <arian.ahmidi@gmail.com>
2024-05-14 23:45:09 +0330

    quick change to the logger class definitions

--- mashtframework/src/main/java/ir/doodmood/mashtframework/core/Logger.java
+++ mashtframework/src/main/java/ir/doodmood/mashtframework/core/Logger.java
@@ -4,9 +4,9 @@ import ir.doodmood.mashtframework.annotation.Component;
 
 @Component(singleton = true)
 public class Logger {
-    public void debug(String message);
-    public void info(String message);
-    public void warning(String message);
-    public void error(String message);
-    public void critical(String message);
+    public void debug(String... message);
+    public void info(String... message);
+    public void warning(String... message);
+    public void error(String... message);
+    public void critical(String... message);
 }

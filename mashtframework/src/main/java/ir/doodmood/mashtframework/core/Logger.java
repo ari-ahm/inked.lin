61360a44a0147bc12cb836659ec26f800e4c6411
mohammad arian ahmidi <arian.ahmidi@gmail.com>
2024-05-14 23:38:42 +0330

    added the base structure of the logger class

---
+++ mashtframework/src/main/java/ir/doodmood/mashtframework/core/Logger.java
@@ -0,0 +1,12 @@
+package ir.doodmood.mashtframework.core;
+
+import ir.doodmood.mashtframework.annotation.Component;
+
+@Component(singleton = true)
+public class Logger {
+    public void debug(String message);
+    public void info(String message);
+    public void warning(String message);
+    public void error(String message);
+    public void critical(String message);
+}

12f24a3804b961d6e3aea91b1b0aab27f42f8740
mohammad arian ahmidi <arian.ahmidi@gmail.com>
2024-05-15 13:58:23 +0330

    last small change to Logger class structure

--- mashtframework/src/main/java/ir/doodmood/mashtframework/core/Logger.java
+++ mashtframework/src/main/java/ir/doodmood/mashtframework/core/Logger.java
@@ -1,12 +1,13 @@
 package ir.doodmood.mashtframework.core;
 
 import ir.doodmood.mashtframework.annotation.Component;
+import ir.doodmood.mashtframework.exception.CriticalError;
 
 @Component(singleton = true)
 public class Logger {
-    public void debug(String... message);
-    public void info(String... message);
-    public void warning(String... message);
-    public void error(String... message);
-    public void critical(String... message);
+    public void debug(Object... message);
+    public void info(Object... message);
+    public void warning(Object... message);
+    public void error(Object... message);
+    public void critical(Object... message) throws CriticalError;
 }
